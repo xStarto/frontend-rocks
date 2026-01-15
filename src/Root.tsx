cd8af74748bd462955a10b26d1b35d00ab93dd71
xStarto <pozzimirko1@gmail.com>
2026-01-15 13:35:03 +0000

    Prima Versione

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,12 +3,13 @@ import { Link } from "react-router";
 
 export const Root = () => {
   return (
-    <div className="pt-10 pl-10 flex justify-start flex-wrap items-start h-screen">
+    <div className="pt-10 pl-10 space-y-10 space-x-10 flex justify-start flex-wrap items-start h-screen">
       <Card title="Bulbasaur" image="https://www.clipartmax.com/png/middle/38-382977_1-pokemon-bulbasaur.png" />
+      <Card title="Charmander" image="https://www.clipartmax.com/png/middle/38-382976_charmander-pokemon-png-image-free-download-charmander-pokemon-png.png" />
     </div>
   );
-  
+
 }
 export const Card = (props : { title: string, image : string }) => {
-  return <div className="bg-gray-300 h-40 w-40 text-up flex justify-center rounded-md" title={props.title} image={props.image}>{props.title}</div>;
+  return <div className="bg-gray-300 h-40 w-40 text-up flex justify-center rounded-md" title={props.title}>{props.title}</div>;
 };

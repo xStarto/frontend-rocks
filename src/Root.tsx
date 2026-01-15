2b2c9575152444804b75af026b1114a3515cc200
xStarto <pozzimirko1@gmail.com>
2026-01-15 13:31:43 +0000

    fix

--- src/Root.tsx
+++ src/Root.tsx
@@ -7,6 +7,7 @@ export const Root = () => {
       <Card title="Bulbasaur" image="https://www.clipartmax.com/png/middle/38-382977_1-pokemon-bulbasaur.png" />
     </div>
   );
+  
 }
 export const Card = (props : { title: string, image : string }) => {
   return <div className="bg-gray-300 h-40 w-40 text-up flex justify-center rounded-md" title={props.title} image={props.image}>{props.title}</div>;
